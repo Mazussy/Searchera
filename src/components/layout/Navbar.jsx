e1df6f72b9c19c2640e4e23afaf8c085b204cc39
AssaultMachete <Assualtmachete20@gmail.com>
2026-01-27 22:47:40 +0200

    ...

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+        <nav className="bg-white border-b-0.5 color-[#4242425C]/36 sticky top-0 z-50">
+            <div className="max-w-7xl mx-auto px-6 lg:px-8">
+                <div className="flex items-center justify-between h-auto">
+                    <div className="shrink-0">
+                        <a href="/" className="text-[30px] font-normal font-alatsi"></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    )
+}
+export default Navbar;
